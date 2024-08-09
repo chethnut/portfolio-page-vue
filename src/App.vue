72a5e0341b3116ee6e019fb7b9e120c7d6278d9c
chethnut <sethchesky@gmail.com>
2024-08-09 13:04:08 -0500

    adding files

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
+</script>
+
+<template>
+  <HeaderComp/>
+  <header>
+    <h1>main view</h1>
+
+    
+      
+
+      
+    
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <header class="header">
+            <h2 class="title">Seth Chesky Portfolio</h2>
+            <h2 class="empty-element"></h2>
+            <h2 class="about-me">About Me</h2>
+            <h2 class="my-projects">My Projects</h2>
+        </header>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+    }
+    .header {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        padding-left: 20px;
+        
+    }
+    .about-me, .my-projects {
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+</style>
