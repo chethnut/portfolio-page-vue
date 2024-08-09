e389b2ec0d5640c6e7d154fe13fa33f6a45bc7a1
chethnut <sethchesky@gmail.com>
2024-08-09 13:53:24 -0500

    fixing repo issues

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,35 +1,58 @@
 <template>
-    <div class="container">
-        <header class="header">
-            <h2 class="title">Seth Chesky Portfolio</h2>
-            <h2 class="empty-element"></h2>
-            <h2 class="about-me">About Me</h2>
-            <h2 class="my-projects">My Projects</h2>
-        </header>
+    <div class="header-container">
+        <div class="container1">
+            <header class="header">
+                <h2 class="title">Seth Chesky Portfolio</h2>
+            </header>
+        </div>
+
+        <div class="container2">
+            <header class="header">
+                <h2 class="about-me">About Me</h2>
+                <h2 class="my-projects">My Projects</h2>
+            </header>
+        </div>
     </div>
+
 </template>
 
 <script>
 </script>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: flex-start;
+.header-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+}
 
-    }
-    .header {
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        align-items: center;
-        padding-left: 20px;
-        
-    }
-    .about-me, .my-projects {
-        padding-left: 20px;
-        padding-right: 20px;
-    }
+.container1 {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+
+.container2 {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding-right: 20px;
+    
+}
+
+.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 20px;
+
+}
+
+.about-me,
+.my-projects {
+    padding-left: 20px;
+}
 </style>
---
+++ src/views/MyProjectsView.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>My Projects View</h1>
+    <h3>component goes here</h3>
+</template>
