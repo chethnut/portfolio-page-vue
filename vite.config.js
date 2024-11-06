75c4bc29263577c9fef7f286a1066d82ffd1bb40
chethnut <sethchesky@gmail.com>
2024-11-06 11:59:15 -0600

    second attempt to fix SPA refresh error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
-    historyApiFallback: true,
+    fs: {
+      strict: false,
+    }
   },
   plugins: [
     vue(),
