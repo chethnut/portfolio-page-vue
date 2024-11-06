599244d078804f00a0b1ff1baccdc69241de95e0
chethnut <sethchesky@gmail.com>
2024-11-06 11:46:21 -0600

    attempting fix for SPA refresh bug

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    historyApiFallback: true,
+  },
   plugins: [
     vue(),
   ],
@@ -13,4 +16,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
